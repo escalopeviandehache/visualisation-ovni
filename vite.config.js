99ed45fda6cb76656221a101f7d1c0e5f4070de1
Kemxr <marcbouriot@hotmail.com>
2025-03-17 15:09:04 +0100

    Structure initialized

---
+++ vite.config.js
@@ -0,0 +1,11 @@
+import { defineConfig } from "vite";
+
+export default defineConfig({
+    root: "src", //Racine de notre projet
+    build : {
+        outDir: "../build", //Change le chemin et le nom du fichier du build
+        // Par défaut : target: "modules"
+        target: "ES2022", //Valide seulement pour les nouveaux navigateur et pas les anciens
+        emptyOutDir: true, //Garde seulement la version du dernier build
+    }
+});
