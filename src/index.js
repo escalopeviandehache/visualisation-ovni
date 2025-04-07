e07cb6815f2915202673504de6c100404b149ce2
escalopeviandehache <kevin.doregodossantos@heig-vd.ch>
2025-04-07 10:56:53 +0200

    Update changes

--- src/index.js
+++ src/index.js
@@ -3,3 +3,4 @@ import '/js/data/processed_data.js';
 import '/js/section/ufo-type-graph-section.js';
 import '/js/section/US-map-section.js';
 import '/js/section/testimony-section.js';
+import '/js/section/slider-evolution.js';
